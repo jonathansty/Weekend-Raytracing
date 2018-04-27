9af0e3d0a60966dac2af05d82eca60a6175b34c2
Jonathan Steyfkens <7695731+jonathansty@users.noreply.github.com>
2018-04-27 08:13:10 +0100

    Chapter 10

--- include/Vec3.h
+++ include/Vec3.h
@@ -1,5 +1,6 @@
 #pragma once
-
+#define _USE_MATH_DEFINES
+#include <math.h>
 class Vec3
 {
 public:
@@ -106,7 +107,7 @@ inline Vec3 cross(const Vec3& v1, const Vec3& v2)
 {
 	return Vec3(
 		v1.e[1]*v2.e[2] - v1.e[2]*v2.e[1],
-		v1.e[0]*v2.e[2] + v1.e[2]*v2.e[0],
+		-( v1.e[0]*v2.e[2] - v1.e[2]*v2.e[0] ),
 		v1.e[0]*v2.e[1] - v1.e[1]*v2.e[0]
 	);
 }
